014ad026fbc3c642ec9a616a2e14ac31532af41d
leejh3233 <leejh3233@nate.com>
2025-06-29 14:42:01 +0900

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return <h1>HairFit 앱에 오신 것을 환영합니다!</h1>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
